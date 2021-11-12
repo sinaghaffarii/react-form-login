2e571e0804acac1b1e3b1f546344bde27a9f6bed
sinaghaffarii <sinaghafari.dev@gmail.com>
2021-11-13 00:24:06 +0330

    add useState and axios to form

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Card, Button, Form } from "react-bootstrap";
+import axios from "axios";
 
 const LoginForm = () => {
+  const [formValue, setFormValue] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    message: "",
+    reminder: [],
+  });
+
+  const [gender, setGender] = useState("0");
+
+  const onChangeHandler = (e) => {
+    console.log(e.target.name);
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
+    // در هدر توکن کاربر را به بک اند ارسال مینمایند تا مشخص بشود هنگام خرید کدام کاربر محصول را خریداری کرده است
+    const header = {
+      headers: {
+        autherization: "Bearer Token",
+      },
+    };
+    // -------- این نوشتن استیت به صورت دوتایی معلوم نیست درست باشد یا نه
+    axios.post("URL", `formValue, gender`, header);
   };
 
   return (
@@ -18,29 +43,89 @@ const LoginForm = () => {
           <Form onSubmit={onSubmitHandler}>
             <Form.Group className="mb-3">
               <Form.Label className="text-primary">First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter First Name" />
+              <Form.Control
+                type="text"
+                name="firstname"
+                value={formValue.firstname}
+                onChange={onChangeHandler}
+                placeholder="Enter First Name"
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label className="text-primary">Enter Lastname</Form.Label>
-              <Form.Control type="text" placeholder="Enter Last Name" />
+              <Form.Control
+                type="text"
+                name="lastname"
+                value={formValue.lastname}
+                onChange={onChangeHandler}
+                placeholder="Enter Last Name"
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label className="text-primary">Email</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Control
+                type="email"
+                name="email"
+                value={formValue.email}
+                onChange={onChangeHandler}
+                placeholder="name@example.com"
+              />
               <Form.Text className="text-whait-50">
                 we'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label className="text-primary">Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter Password" />
+              <Form.Control
+                type="password"
+                name="password"
+                value={formValue.password}
+                onChange={onChangeHandler}
+                placeholder="Enter Password"
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label className="text-primary">About you</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control
+                as="textarea"
+                name="message"
+                value={formValue.message}
+                onChange={onChangeHandler}
+                rows={3}
+              />
             </Form.Group>
+            {/* -------------------------------------------------------------------------- Radio --------------------------- */}
+            <div className="d-flex">
+              <Form.Group className="mb-3 me-4">
+                <Form.Check
+                  type="radio"
+                  name="gender"
+                  value="0"
+                  checked={gender === "0"}
+                  onChange={(e) => setGender(e.target.value)}
+                  label="Male"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Check
+                  type="radio"
+                  name="gender"
+                  value="1"
+                  checked={gender === "1"}
+                  onChange={(e) => setGender(e.target.value)}
+                  label="Female"
+                />
+              </Form.Group>
+            </div>
+            {/* ------------------------------------------------------------------------- CheckBox ------------------------------------------- */}
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="remember me" />
+              <Form.Check
+                type="checkbox"
+                name="reminder"
+                value={formValue.reminder}
+                onChange={onChangeHandler}
+                label="remember me"
+              />
             </Form.Group>
             <Button type="submit" className="w-100 text-center ">
               submit
